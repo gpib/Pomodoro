9157b522eb57238209a46f7524d478746aec762c
gpib <rafal.koslowski@gmail.com>
2024-12-29 11:32:56 +0100

    added GUI objects to the main pomodoro screen

--- main.py
+++ main.py
@@ -10,19 +10,35 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_pomodoro():
+    pass
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_pomodoro():
+    pass
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+
+title_label = Label(text = "Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=YELLOW, highlightthickness=0)
+title_label.grid(column=1, row=0)
+
+button_start = Button(text = "Start", command = start_pomodoro)
+#button.pack()
+button_start.grid(column = 0, row = 2)
+button_start.config(padx = 5, pady = 5)
+
+button_reset = Button(text = "Reset", command = reset_pomodoro)
+#button.pack()
+button_reset.grid(column = 2, row = 2)
+button_reset.config(padx = 5, pady = 5)
+
 canvas = Canvas(width = 200, height = 224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image = tomato_img )
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
 window.mainloop()
