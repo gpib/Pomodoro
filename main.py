77c525982f4fbec06c022d687e59cae7bbba4def
gpib <rafal.koslowski@gmail.com>
2025-01-12 09:44:53 +0100

    A counter was added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import time
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,13 +11,25 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 def reset_pomodoro():
     pass
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_pomodoro():
-    pass
+    count_down(5*60)
+
+def exit_pomodoro():
+    window.destroy()
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = count // 60
+    count_secs = count % 60
+    time_format = f"{count_min}:{count_secs:02d}"
+    canvas.itemconfig(timer_count, text = time_format)
+    if count > 0:
+        window.after(1000, count_down, count -1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,19 +40,26 @@ title_label = Label(text = "Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=Y
 title_label.grid(column=1, row=0)
 
 button_start = Button(text = "Start", command = start_pomodoro)
-#button.pack()
 button_start.grid(column = 0, row = 2)
 button_start.config(padx = 5, pady = 5)
 
 button_reset = Button(text = "Reset", command = reset_pomodoro)
-#button.pack()
 button_reset.grid(column = 2, row = 2)
 button_reset.config(padx = 5, pady = 5)
+
+button_exit = Button(text = "Exit", command = exit_pomodoro)
+button_exit.grid(column = 1, row = 4)
+button_exit.config(padx = 5, pady = 5)
+
+check_label = Label(text = "✔", fg=GREEN, font=(FONT_NAME, 15, "bold"), bg=YELLOW, highlightthickness=0)
+check_label.grid(column=1, row=3)
 
 canvas = Canvas(width = 200, height = 224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image = tomato_img )
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_count = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
+
+
 
 window.mainloop()
